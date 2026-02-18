d841bb5d8e38ed5adbc6fbaf86f7238814fab25d
shamshodbekdevops <shamshodbekdevops@users.noreply.github.com>
2026-02-18 19:12:15 +0500

    Stabilize Railway worker by limiting Celery concurrency

--- backend/start-worker.sh
+++ backend/start-worker.sh
@@ -2,5 +2,10 @@
 set -e
 
 export C_FORCE_ROOT=${C_FORCE_ROOT:-true}
+CELERY_CONCURRENCY=${CELERY_CONCURRENCY:-2}
+CELERY_POOL=${CELERY_POOL:-prefork}
 
-exec celery -A config worker -l ${CELERY_LOG_LEVEL:-info}
+exec celery -A config worker \
+  -l ${CELERY_LOG_LEVEL:-info} \
+  --pool "${CELERY_POOL}" \
+  --concurrency "${CELERY_CONCURRENCY}"
